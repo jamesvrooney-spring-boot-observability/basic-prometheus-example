fb4d7ea9bcf8a67df22c64aa93ef38873430457b
jamesvrooney <jamesvrooney@hotmail.com>
2023-01-27 12:07:57 +0000

    Initial commit.

---
+++ src/main/java/com/jamesvrooney/BasicPrometheusExampleApplication.java
@@ -0,0 +1,13 @@
+package com.jamesvrooney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicPrometheusExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicPrometheusExampleApplication.class, args);
+	}
+
+}
